e2f899585d07dad140a661274a0b5c040ea3ad1a
satyam1614 <satyamyadav162000@.com>
2026-06-06 12:50:57 +0530

    ORSProject-V04

---
+++ ORSProject04/src/main/java/com/sunilos/p4/model/ProductModel.java
@@ -0,0 +1,138 @@
+package com.sunilos.p4.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.sunilos.p4.bean.ProductBean;
+import com.sunilos.p4.bean.UserBean;
+import com.sunilos.p4.exception.ApplicationException;
+import com.sunilos.p4.exception.DuplicateRecordException;
+import com.sunilos.p4.util.JDBCDataSource;
+
+public class ProductModel extends BaseModel<ProductBean> {
+
+	@Override
+	public ProductBean getBean() {
+		return new ProductBean();
+	}
+
+	@Override
+	public long add(ProductBean bean) throws ApplicationException, DuplicateRecordException {
+		log.debug("Model add Started");
+		Connection conn = null;
+		int pk = 0;
+
+		ProductBean existbean = findByProductName(bean.getProductName());
+
+		if (existbean != null) {
+			throw new DuplicateRecordException("productName already exists");
+		}
+
+		try {
+			conn = JDBCDataSource.getConnection();
+			pk = nextPK();
+			// Get auto-generated next primary key
+			System.out.println(pk + " in ModelJDBC");
+			conn.setAutoCommit(false); // Begin transaction
+			PreparedStatement pstmt = conn.prepareStatement("INSERT INTO " + getTable() + " VALUES(?,?,?,?,?,?,?,?,?)");
+			pstmt.setInt(1, pk);
+			pstmt.setString(2, bean.getProductName());
+			pstmt.setString(3, bean.getProductCategory());
+			pstmt.setDate(4, new java.sql.Date(bean.getOrderDate().getTime()));
+			pstmt.setInt(5, bean.getPrice());
+			pstmt.setString(6, bean.getCreatedBy());
+			pstmt.setString(7, bean.getModifiedBy());
+			pstmt.setTimestamp(8, bean.getCreatedDatetime());
+			pstmt.setTimestamp(9, bean.getModifiedDatetime());
+			pstmt.executeUpdate();
+			conn.commit(); // End transaction
+			pstmt.close();
+		} catch (Exception e) {
+			log.error("Database Exception..", e);
+			try {
+				conn.rollback();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				throw new ApplicationException("Exception : add rollback exception " + ex.getMessage());
+			}
+			throw new ApplicationException("Exception : Exception in add User");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+		log.debug("Model add End");
+		return pk;
+	}
+
+	@Override
+	public void update(ProductBean bean) throws ApplicationException, DuplicateRecordException {
+		log.debug("Model update Started");
+		Connection conn = null;
+
+		ProductBean beanExist = findByProductName(bean.getProductName());
+		// Check if updated LoginId already exist
+		if (beanExist != null && !(beanExist.getId() == bean.getId())) {
+			throw new DuplicateRecordException("ProductName is already exist");
+		}
+
+		try {
+			conn = JDBCDataSource.getConnection();
+			conn.setAutoCommit(false); // Begin transaction
+			PreparedStatement pstmt = conn.prepareStatement(
+					"UPDATE ST_USER SET PRODUCT_NAME=?,PRODUCT_CATEGORY=?,ORDER_DATE=?,PRICE=?,CREATED_BY=?,MODIFIED_BY=?,CREATED_DATETIME=?,MODIFIED_DATETIME=? WHERE ID=?");
+			pstmt.setString(1, bean.getProductName());
+			pstmt.setString(2, bean.getProductCategory());
+			pstmt.setDate(3, new java.sql.Date(bean.getOrderDate().getTime()));
+			pstmt.setInt(4, bean.getPrice());
+			pstmt.setString(5, bean.getCreatedBy());
+			pstmt.setString(6, bean.getModifiedBy());
+			pstmt.setTimestamp(7, bean.getCreatedDatetime());
+			pstmt.setTimestamp(8, bean.getModifiedDatetime());
+			pstmt.setLong(9, bean.getId());
+			pstmt.executeUpdate();
+			conn.commit(); // End transaction
+			pstmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("Database Exception..", e);
+			try {
+				conn.rollback();
+			} catch (Exception ex) {
+				throw new ApplicationException("Exception : Delete rollback exception " + ex.getMessage());
+			}
+			throw new ApplicationException("Exception in updating User ");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+		log.debug("Model update End");
+	}
+
+	
+
+	@Override
+	public String getWhereClause(ProductBean bean) {
+
+		StringBuffer sql = new StringBuffer();
+
+		if (bean != null) {
+			if (bean.getId() > 0) {
+				sql.append(" AND id = " + bean.getId());
+			}
+			if (bean.getProductName() != null && bean.getProductName().length() > 0) {
+				sql.append(" AND PRODUCT_NAME like '" + bean.getProductName() + "%'");
+			}
+		}
+
+		return sql.toString();
+	}
+
+	public ProductBean findByProductName(String productName) {
+		return findByUniqueColumn("PRODUCT_NAME", productName);
+	}
+
+	@Override
+	public String getTable() {
+		return "st_product";
+	}
+
+}
+
---
+++ ORSProject04/src/main/java/com/sunilos/p4/model/test/TestProductModel.java
@@ -0,0 +1,12 @@
+package com.sunilos.p4.model.test;
+
+public class TestProductModel {
+	
+	public static void main(String[] args) {
+		
+	
+		
+		
+	}
+
+}
