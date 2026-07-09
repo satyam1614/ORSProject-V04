43bd950ac35335660285e4fcdd4c37a56c02db8a
satyam1614 <satyamyadav162000@.com>
2026-07-09 12:56:26 +0530

    Add database down condition

--- ORSProject04/src/main/java/com/sunilos/p4/ctl/BaseCtl.java
+++ ORSProject04/src/main/java/com/sunilos/p4/ctl/BaseCtl.java
@@ -1,6 +1,7 @@
 package com.sunilos.p4.ctl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -163,6 +164,15 @@ public abstract class BaseCtl<B extends BaseBean, M extends BaseModel> extends H
 			// Handle if any duplicate record exception
 			ServletUtility.setBean(bean, request);
 			ServletUtility.setErrorMessage(e.getMessage(), request);
+			ServletUtility.forwardPage(getView(), request, response);
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage() + " ============== database is down ===================");
+			ServletUtility.setPageNo(1, request);
+			ServletUtility.setPageSize(10, request);
+			ServletUtility.setList(new ArrayList<BaseBean>(), request);
+			request.setAttribute("nextList", new ArrayList<BaseBean>());
+			ServletUtility.setErrorMessage("Database Server down", request);
 			ServletUtility.forwardPage(getView(), request, response);
 		}
 
